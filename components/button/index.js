3e67e17eb8cfbd708d5c37f5b2649fc867d8feeb
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-06-24 12:00:38 +0200

    feat: refactor Button component for enhanced lifecycle handling using initializeController, bump version to 0.0.6

--- components/button/index.js
+++ components/button/index.js
@@ -1,48 +1,61 @@
-import { WebuumElement } from 'webuum'
+import { initializeController } from 'webuum'
 
-export class Button extends WebuumElement {
-  static props = {
-    $loading: true,
+/**
+ * @param {CustomElementConstructor} Element
+ */
+export const Button = Element =>
+  /**
+   * @property {boolean} $loading
+   */
+  class extends Element {
+    static props = {
+      $loading: true,
+    }
+
+    $loadingAttribute = 'data-loading'
+    $spinnerSelector = '.spinner-absolute'
+    $spinnerHTML = `<span class="spinner spinner-absolute"></span>`
+
+    constructor() {
+      super()
+
+      initializeController(this)
+    }
+
+    connectedCallback() {
+      this.$observer = new MutationObserver(() => {
+        const isLoading = this.hasAttribute(this.$loadingAttribute)
+
+        if (isLoading && this.querySelector(this.$spinnerSelector)) return
+
+        if (isLoading) {
+          this.insertAdjacentHTML('beforeend', this.$spinnerHTML)
+        }
+        else {
+          this.querySelector(this.$spinnerSelector).remove()
+        }
+
+        this.disabled = isLoading
+      })
+
+      this.$loading && this.$observer.observe(this, {
+        attributeFilter: [this.$loadingAttribute],
+      })
+
+      this.addEventListener('click', this.ripple)
+    }
+
+    disconnectedCallback() {
+      this.$observer.disconnect()
+    }
+
+    async ripple({ currentTarget, offsetX, offsetY }) {
+      const { showRipple } = await import('winduum/src/utilities/ripple')
+
+      showRipple({
+        currentTarget,
+        offsetX,
+        offsetY,
+      })
+    }
   }
-
-  $loadingAttribute = 'data-loading'
-  $spinnerSelector = '.spinner-absolute'
-  $spinnerHTML = `<span class="spinner spinner-absolute"></span>`
-
-  connectedCallback() {
-    this.$observer = new MutationObserver(() => {
-      const isLoading = this.hasAttribute(this.$loadingAttribute)
-
-      if (isLoading && this.querySelector(this.$spinnerSelector)) return
-
-      if (isLoading) {
-        this.insertAdjacentHTML('beforeend', this.$spinnerHTML)
-      }
-      else {
-        this.querySelector(this.$spinnerSelector).remove()
-      }
-
-      this.disabled = isLoading
-    })
-
-    this.$loading && this.$observer.observe(this, {
-      attributeFilter: [this.$loadingAttribute],
-    })
-
-    this.addEventListener('click', this.ripple)
-  }
-
-  disconnectedCallback() {
-    this.$observer.disconnect()
-  }
-
-  async ripple({ currentTarget, offsetX, offsetY }) {
-    const { showRipple } = await import('winduum/src/utilities/ripple')
-
-    showRipple({
-      currentTarget,
-      offsetX,
-      offsetY,
-    })
-  }
-}
