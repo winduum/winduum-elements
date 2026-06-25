e5ce274954f8550dcf6dc74b55211a41317897e5
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-06-25 16:44:05 +0200

    feat: refactor Toast component lifecycle methods for consistency with Webuum standards

--- components/toast/index.js
+++ components/toast/index.js
@@ -1,20 +1,31 @@
 import { WebuumElement } from 'webuum'
 
+/**
+ * @typedef {import('winduum/src/components/toast/index.js').ShowToastOptions} ShowOptions
+ * @typedef {import('winduum/src/components/toast/index.js').CloseToastOptions} CloseOptions
+ */
+
 export class Toast extends WebuumElement {
+  /**
+   * @type {ShowOptions}
+   */
   $showOptions
+  /**
+   * @type {CloseOptions}
+   */
   $closeOptions
 
   connectedCallback() {
-    this.showToast()
+    this.show()
   }
 
-  async showToast() {
+  async show() {
     const { showToast } = await import('winduum/src/components/toast/index.js')
 
     await showToast(this, this.$showOptions)
   }
 
-  async closeToast() {
+  async close() {
     const { closeToast } = await import('winduum/src/components/toast/index.js')
 
     await closeToast(this, this.$closeOptions)
